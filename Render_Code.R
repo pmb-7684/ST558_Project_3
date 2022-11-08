6a0e222f229d5bb05c831b05cb78b3eba3890fbe
Paula M-Bailey <pbailey21@outlook.com>
2022-11-08 05:38:13 -0500

    Commit Test and Render - work

--- Render_Code.R
+++ Render_Code.R
@@ -1,3 +1,19 @@
-rmarkdown::render("TestOne.Rmd", output_file = "LifestyleAnalysis.html",params = list(socialID = "Lifestyle"))
+selectID <- unique(newData$channel)  # from notes this should work
 
+#create filename
+output_file <- paste0(selectID, "Analysis.html")   #should be .md for the project
+#create a group for each team with just the team name parameter
+params = lapply(selectID, FUN = function(x){list(channel = x)})
 
+#put into a data frame
+reports <- tibble(output_file, params)
+
+library(rmarkdown)
+
+apply(reports, MARGIN = 1,
+      FUN = function(x){
+        render(input = "./TestOne.Rmd",
+               output_format = "github_document", 
+               output_file = x[[1]], 
+               params = x[[2]])
+      })
